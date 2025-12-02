7256d7fb5e72b6f26d0c1e01a20592e3c1954c02
Subroto <subroto.2003@gmail.com>
2025-12-03 00:06:45 +0530

    feat: started tweaking UI

--- lib/pages/clock_screen.dart
+++ lib/pages/clock_screen.dart
@@ -6,7 +6,33 @@ class ClockScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/bg.jpg',
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+            ),
+
+            Positioned(
+              top: MediaQuery.of(context).size.height/2,
+              left: MediaQuery.of(context).size.width/2,
+              child: Container(
+                
+                child: Column(
+                  mainAxisAlignment: .center,
+                  crossAxisAlignment: .center,
+                  children: [
+                    Text('Time nigga', style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
